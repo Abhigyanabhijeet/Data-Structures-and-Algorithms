a5517a03b6fe445d45c21a9f73a8755d6a26681e
Abhigyanabhijeet <abhigyanabhijeet@gmail.com>
2018-04-02 08:58:33 +0530

    Update MaxPairwiseProduct.py

--- MaxPairwiseProduct.py
+++ MaxPairwiseProduct.py
@@ -1,21 +1,14 @@
-
-# coding: utf-8
-
-# In[27]:
-
-
 # Uses python3
 n = int(input())
 a = [int(x) for x in input().split()]
 assert(len(a) == n)
 a.sort()
-for index in range(n-2,0,-1):
+for index in range(n-2,-1,-1):
     if a[index] != a[n-1]:
         result = a[n-1]*a[index]
+        print (result)
         break
 
 
 
-
-print(result)
 

47695e52870dad2b066bf3a7b9b468a7da979471
Abhigyanabhijeet <abhigyanabhijeet@gmail.com>
2018-04-10 22:58:10 +0530

    C++ Code Only

---
+++ 1_money_change/change.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int count=0;
+int get_change(int m) {
+  if (m>=10){
+    count+=1;
+    return(get_change(m-10));
+  }
+  else if(m>=5){
+    count+=1;
+    return(get_change(m-5));
+  }
+  else if(m>=1){
+    count+=1;
+    return(get_change(m-1));
+  }
+  else{
+    return (count);
+  }
+}
+
+int main() {
+  int m;
+  std::cin >> m;
+  std::cout << get_change(m) << '\n';
+}
